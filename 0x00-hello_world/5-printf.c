78152e9f6145647af88838f157e6af612eab41f4
Daniel Ulelu <danielulelu@gmail.com>
2023-03-22 05:18:12 -0700

    printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - program to print a line using printf function
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper gramer, but the outcome is a piece of art, \n");
+	return (0);
+}
