00809bc165dc01cce0aaa4ba6e12a4a5d285a40d
Daniel Ulelu <danielulelu@gmail.com>
2023-03-26 22:18:01 -0700

    dereferencing pointers

---
+++ 0x00-hello_world/1-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - addresses of variables
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char c;
+	int n;
+
+	printf("Address of varivale 'c': %p\n", &c);
+	printf("Address of variable 'n':%p\n", &n);
+	return (0);
+}
